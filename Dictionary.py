3533740b5d4fc57b048106a008e30da77e4d9922
akhachat <akhachat@softserveinc.com>
2021-09-28 19:37:14 +0300

    initial commit

---
+++ Dictionary.py
@@ -0,0 +1,49 @@
+choice = input("A or B or C?\n")
+input_text= open('D:\\Projects\\Ddum\\HomeWork\\Dictionary\\test.txt', 'r')
+alpha = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'
+alpha1 = 'abcdefghijklmnopqrstuvwxyz'
+class Dictionary():
+
+    def choice(self, text):
+        if choice == 'A' or choice == 'a':
+            self.choice_A(text)
+        elif choice == 'B' or choice == 'b':
+            self.choice_B(text)
+        elif choice == 'C' or choice == 'c':
+            self.choice_C(text)
+        else:
+            print('dibloid')
+        
+    def colculate_total_count(self, text):
+        count_letters = 0
+        for i in alpha:
+            count = text.count(i)
+            count_letters = count_letters + count
+        return count_letters
+
+    def choice_A(self, text):
+        list_txt_A = text.split(' ')
+        list_txt_A.sort()
+        list_txt_A = ', '.join(list_txt_A)
+        print(list_txt_A)
+        
+    
+    def choice_B(self, text):
+        for i in alpha1:
+            count = text.count(i) + text.count(i.upper())
+            if count == 0:
+                continue
+            print(("{} - {}".format(i.upper(), count)))
+        
+
+
+    def choice_C(self, text):
+        totalLen = self.colculate_total_count(text)
+        for j in alpha1:
+            count = text.count(j) + text.count(j.upper())
+            percent = "{:.2f}".format(round((count/totalLen*100),2))
+            print(("{0} - {1}%".format(j.upper(), percent)))
+
+
+object1 = Dictionary()
+object1.choice(input_text.read())
---
+++ Dictionary0.2.py
@@ -0,0 +1,80 @@
+from tkinter import *  
+alpha = 'abcdefghijklmnopqrstuvwxyz'
+
+def DoneA():
+    txt.focus()
+    btnDone = Button(window, text="Done", command=choice_A)
+    btnDone.grid(column=1, row=6) 
+
+def DoneB():
+    txt.focus()
+    btnDone = Button(window, text="Done", command=choice_B)
+    btnDone.grid(column=1, row=6) 
+
+def DoneC():
+    txt.focus()
+    btnDone = Button(window, text="Done", command=choice_C)
+    btnDone.grid(column=1, row=6) 
+
+def choice_A():
+    list_txt_A = txt.get()
+    list_txt_A = list_txt_A.split(" ")
+    list_txt_A.sort()
+    list_txt_A = ', '.join(list_txt_A)
+    lbl_A = Label(window, text=list_txt_A, font=("Arial Bold", 10))  
+    lbl_A.grid(column=0, row=7)
+
+def choice_B():
+    list_txt_B = txt.get()
+    x=7
+    for i in alpha:
+        count = list_txt_B.count(i) + list_txt_B.count(i.upper())
+        if count == 0:
+            continue
+        lbl_B = Label(window, text=(("{} - {}".format(i.upper(), count))))
+        lbl_B.grid(column=0, row=x)
+        x+=1
+
+def choice_C():
+    count_letters = 0
+    text = txt.get()
+    x = 7
+    for i in alpha:
+        count_letter = text.count(i)
+        count_letters = count_letters + count_letter
+    totalLen = count_letters
+    for j in alpha:
+        count = text.count(j) + text.count(j.upper())
+        if count == 0:
+            continue
+        formating = "{:.2f}".format(round((count/totalLen*100),2))
+        percent = (("{0},{1} - {2}%".format(j.upper(), j, formating)))
+        lbl_C = Label(window, text=percent, font=("Arial Bold", 10))
+        lbl_C.grid(column=0, row=x)
+        x+=1
+    
+  
+window = Tk()  
+window.title("Dictionary")  
+window.geometry('400x450')
+
+
+txt = Entry(window,width=10)  
+txt.grid(column=0, row=5) 
+
+
+lblor1 = Label(window, text='or', font=("Arial Bold", 10))
+lblor1.grid(column=0, row=1)
+lblor2 = Label(window, text='or', font=("Arial Bold", 10))
+lblor2.grid(column=0, row=3)
+
+btnA = Button(window, text="A", command=DoneA, bg="red", fg="green")
+btnA.place(width=1000, height=1000)
+btnA.grid(column=0, row=0) 
+btnB = Button(window, text="B", command=DoneB, bg="blue", fg="yellow")
+btnB.grid(column=0, row=2)
+btnC = Button(window, text="C", command=DoneC, bg="orange", fg="violet")
+btnC.grid(column=0, row=4)
+
+ 
+window.mainloop()
